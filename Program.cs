5629eb4a1abff15e00dbe1e659af972e38098031
LuisJC96 <luisjuancenturion@gmail.com>
2019-09-05 20:24:33 -0500

    First

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiniMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ivan es puto");
+            
+            char[,] Board = new char[3,3];
+            for(int i = 0; i<3; i++)
+            {
+                for(int j = 0; j<3; j++)
+                {
+                    Board[i,j] = 'a';
+                    Console.WriteLine(Board[i, j]);
+                }
+            }
+            Board board = new Board(Board);
+            board.PrindBoard();
+            Console.ReadKey();
+        }
+    }
+    public class Node
+    {
+        char[,] Board;
+        List<Node> PosibleStates = new List<Node>();
+        public Node(List<Node> posibleStates, char[,] board)
+        {
+            PosibleStates = posibleStates;
+            Board = board;   
+        }
+    }
+    public class Board
+    {
+        char[,] State;
+        public Board(char[, ] state)
+        {
+            State = state;
+        }
+        public  void PrindBoard()
+        {
+            for(int i = 0; i<3; i++)
+            {
+                for(int j = 0; j<3; j++)
+                {
+                    Console.WriteLine(State[i,j]);
+                }
+            }
+        }
+    }
+
+}
